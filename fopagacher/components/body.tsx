71276fb5e54846c8ee2c77a951ab7bdf8b8fb78d
Steven ZABANDITH <steven.zabandith@epitech.eu>
2023-04-20 23:47:31 +0200

    [ADD] components

---
+++ fopagacher/components/body.tsx
@@ -0,0 +1,10 @@
+function Body()
+{
+  return  (
+    <div>
+      <h1>Body components</h1>
+    </div>
+  );
+}
+
+export default Body;
---
+++ fopagacher/components/header.tsx
@@ -0,0 +1,13 @@
+function Header()
+{
+    return (
+        <ul>
+          <li>About</li>
+          <li>ROADMAP</li>
+          <li>Team</li>
+          <li>FAQ</li>
+        </ul>
+    );
+}
+
+export default Header;
