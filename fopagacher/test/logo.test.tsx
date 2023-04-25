05042c3aface12b3ad6d649f8f5e396a15095100
Steven ZABANDITH <steven.zabandith@epitech.eu>
2023-04-25 15:58:25 +0200

    [FIX] simple unit test logo

--- fopagacher/test/logo.test.tsx
+++ fopagacher/test/logo.test.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
+import { render } from '@testing-library/react';
 import Logo from '../components/logo';
 
 describe('Logo Component', () => {
-  it('should render correctly', () => {
-    const component = renderer.create(<Logo />);
-    const tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
+  it('renders the correct title text', () => {
+    const { getByText } = render (<Logo />);
+    const titleElement = getByText('logo components');
+    expect(titleElement).toBeInTheDocument();
   });
 });
